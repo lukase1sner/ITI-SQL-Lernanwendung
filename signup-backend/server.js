5f8646a4815264e9b44f99fbf816fd509009952d
Lukas Eisner <lukas.eisner@studmail.w-hs.de>
2025-04-28 10:38:56 +0200

    Neue Version

--- signup-backend/server.js
+++ signup-backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bcrypt = require('bcrypt');  // Import von bcrypt für das Passwort-Hashing
+const bcrypt = require('bcryptjs');  // Import von bcrypt für das Passwort-Hashing
 
 const app = express();
 const port = 3000;
